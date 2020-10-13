6ae6c6cdf8afc5c65fd34c27fcd403582b10aec0
Luis Miguel Cabrera Garcia <ce_1940@hotmail.es>
2020-10-12 20:10:24 -0400

    Primeros Pasos

---
+++ ScoreZone.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ScoreZone.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
